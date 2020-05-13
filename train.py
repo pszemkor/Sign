a786f5160694a3ba82cc1c36f66a7ad1444e1a2b
Przemysław Jabłecki <prz.jab98@gmail.com>
2020-05-13 18:23:35 +0200

    Add initial model training

---
+++ train.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Conv2D,MaxPooling2D, Dense,Flatten, Dropout
+
+def train_model():
+    train = pd.read_csv('sign-language-mnist/sign_mnist_test/sign_mnist_test.csv')
+    test = pd.read_csv('sign-language-mnist/sign_mnist_train/sign_mnist_train.csv')
+
+    y_train = train['label'].values
+    y_test = test['label'].values
+
+    X_train = train.drop(['label'], axis=1)
+    X_test = test.drop(['label'], axis=1)
+
+    X_train = np.array(X_train.iloc[:, :])
+    X_train = np.array([np.reshape(i, (28, 28)) for i in X_train])
+
+    X_test = np.array(X_test.iloc[:, :])
+    X_test = np.array([np.reshape(i, (28, 28)) for i in X_test])
+
+    num_classes = 26
+    y_train = np.array(y_train).reshape(-1)
+    y_test = np.array(y_test).reshape(-1)
+
+    y_train = np.eye(num_classes)[y_train]
+    y_test = np.eye(num_classes)[y_test]
+
+    X_train = X_train.reshape((27455, 28, 28, 1))
+    X_test = X_test.reshape((7172, 28, 28, 1))
+
+    classifier = Sequential()
+    classifier.add(Conv2D(filters=8, kernel_size=(3, 3), strides=(1, 1), padding='same', input_shape=(28, 28, 1),
+                          activation='relu', data_format='channels_last'))
+    classifier.add(MaxPooling2D(pool_size=(2, 2)))
+    classifier.add(Conv2D(filters=16, kernel_size=(3, 3), strides=(1, 1), padding='same', activation='relu'))
+    classifier.add(Dropout(0.5))
+    classifier.add(MaxPooling2D(pool_size=(4, 4)))
+    classifier.add(Dense(128, activation='relu'))
+    classifier.add(Flatten())
+    classifier.add(Dense(26, activation='softmax'))
+    classifier.compile(optimizer='SGD', loss='categorical_crossentropy', metrics=['accuracy'])
+    classifier.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    classifier.fit(X_train, y_train, epochs=50, batch_size=100)
+    accuracy = classifier.evaluate(x=X_test, y=y_test, batch_size=32)
+    print("Accuracy: ", accuracy[1])
+    classifier.save('CNNmodel.h5')
+
+train_model()
+
