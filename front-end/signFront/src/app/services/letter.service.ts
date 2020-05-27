9cc95ef68900c0ad22efe170543344992def6747
Przemysław Jabłecki <prz.jab98@gmail.com>
2020-05-27 17:25:16 +0200

    Add base url of server

--- front-end/signFront/src/app/services/letter.service.ts
+++ front-end/signFront/src/app/services/letter.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
-
+import {baseURL} from '../shared/base'
 @Injectable({
   providedIn: 'root'
 })
 export class LetterService {
-
   constructor() { }
+
+  getLetterToRecognize() {
+    return 'A'
+  }
 }
