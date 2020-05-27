80a792c272309f24d1329280f634bf8de7b4095e
Przemysław Jabłecki <prz.jab98@gmail.com>
2020-05-27 17:29:44 +0200

    Add endpoint for current letter to recognize

--- front-end/signFront/src/app/services/letter.service.ts
+++ front-end/signFront/src/app/services/letter.service.ts
@@ -1,12 +1,17 @@
 import { Injectable } from '@angular/core';
 import {baseURL} from '../shared/base'
+import { Observable, of } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 @Injectable({
   providedIn: 'root'
 })
 export class LetterService {
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  getLetterToRecognize() {
-    return 'A'
+  getLetterToRecognize(): Observable<string> {
+    return this.http.get<string>(baseURL + "currentletter");
   }
+
 }
